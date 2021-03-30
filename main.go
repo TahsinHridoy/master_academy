54c28bb320c5d3e535acfb7878b0a9964db35ec0
TAHSIN_HASAN <[tahsin.heart@gmail.com]>
2021-03-30 22:46:18 +0600

    array data type

---
+++ main.go
@@ -0,0 +1,34 @@
+
+package main
+
+import (
+"fmt"
+)
+
+func main() {
+//primitive datatype
+
+//rune byte = alias
+// string, int, float32, bool
+//composite datatype
+
+//array
+//bracker,braces,parenthesis.
+//var students [3]string 
+//data set or data assign
+//students[0]="asgor"
+//students[1]="nasarul"
+//students[2]="jahid"
+//data get data pull data retreive
+//fmt.Println(students[1])
+//slice
+//map
+//struct
+
+//fmt.Println(students)
+//fmt.Println(len(students))
+//string literals
+atudents := [...]string{"asgor", "maji", "nihat", "hadi"}
+fmt.Println(len(atudents))
+
+}
