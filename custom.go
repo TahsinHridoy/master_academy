af69c0a7997823fc5f9103b35a28ecd0ddcf1bcb
TAHSIN_HASAN <[tahsin.heart@gmail.com]>
2021-04-01 17:01:03 +0600

    my struct

---
+++ custom.go
@@ -0,0 +1,35 @@
+package main
+
+type weight float32
+
+var name string = "mostain"
+
+
+ //struct
+// user defind datatype
+/*
+type Book struct{
+title string
+author string
+ISBN string
+price float32
+pages int
+
+}
+*/
+/*
+// struct literals
+b1 := struct{
+title string
+author string
+ISBN string
+price float32
+pages int
+}{
+title : "An introduction of programing in go",
+author : "caleb doxy",
+pages : 165,
+ISBN : "350987-8754",
+}
+*/
+//fmt.Println(b1.price)
---
+++ main.go
@@ -0,0 +1,13 @@
+package main 
+import "fmt"
+
+var w weight
+
+func main() {
+w = 34.50
+
+fmt.Println(w, name)
+
+
+}
+
