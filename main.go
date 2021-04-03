386cc82ae6674ecea75ef90d7bad75c477e1be70
TAHSIN_HASAN <[tahsin.heart@gmail.com]>
2021-04-03 15:20:57 +0600

    flowcontrol

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+func main() {
+/*
+fmt.Print("enter your age: ")
+var age int
+fmt.Scanf("%d", &age)
+
+if age < 3 {
+fmt.Println("infant")
+
+}else if age > 2 && age<13 {
+fmt.Println("children")
+}else if age>12 && age <=19 {
+fmt.Println("young")
+
+}else {
+fmt.Println("adult")
+}
+
+//fixt value
+switch age {
+case 0,1,2:
+fmt.Println("infant")
+fallthrough
+
+case 3,4,5,6,7,8,9,10,11,12:
+fmt.Println("children")
+
+case 13,14,15,16,17,18,19:
+fmt.Println("young")
+
+default:
+fmt.Println("adult")
+}
+
+
+//for loop
+for i:=1; i<=9; i++ {
+fmt.Println(i)
+}
+
+students := []string{"jack", "jhon", "jones"} 
+for i, std := range students {
+fmt.Println(i, std)
+}
+*/
+//boolian expression
+i := 0
+for i<10 {
+     fmt.Println(i, "hello")
+  i++
+}
+//fmt.Println(age)
+}
